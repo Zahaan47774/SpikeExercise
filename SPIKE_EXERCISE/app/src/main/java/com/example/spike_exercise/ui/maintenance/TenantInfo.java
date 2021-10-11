5ed376a2a4f2598bf5d64cfc1e94bd481119829e
raeafr7 <raeafr7@gmail.com>
2021-10-10 20:13:55 -0500

    trying to fix set response for maintenance

--- SPIKE_EXERCISE/app/src/main/java/com/example/spike_exercise/ui/maintenance/TenantInfo.java
+++ SPIKE_EXERCISE/app/src/main/java/com/example/spike_exercise/ui/maintenance/TenantInfo.java
@@ -1,10 +1,20 @@
 package com.example.spike_exercise.ui.maintenance;
 
 public class TenantInfo {
-    public String tenantID,companyName;
+    public String tenantID,companyName,requestID;
+
+    public TenantInfo(String tenantID, String companyName, String requestID){
+        this.tenantID = tenantID;
+        this.requestID = requestID;
+        this.companyName = companyName;
+    }
     public TenantInfo(String tenantID, String companyName){
         this.tenantID = tenantID;
         this.companyName = companyName;
+    }
+
+    public String getRequestID() {
+        return requestID;
     }
 
     public String getTenantID() {
