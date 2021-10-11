b2f66fe1148822c58b87647e47ea54d27e300a9a
masranber <47961042+masranber@users.noreply.github.com>
2021-10-10 22:23:35 -0500

    fix tenant account

--- SPIKE_EXERCISE/app/src/main/java/com/example/spike_exercise/data/model/TenantAccount.java
+++ SPIKE_EXERCISE/app/src/main/java/com/example/spike_exercise/data/model/TenantAccount.java
@@ -7,7 +7,7 @@ import com.example.spike_exercise.data.AccountType;
 public class TenantAccount extends Account {
 
     public TenantAccount(String uid, String propertyManager, String firstName, String lastName) {
-        super(uid, propertyManager, firstName, lastName, AccountType.TENANT);
+        super(uid, propertyManager, null, firstName, lastName, AccountType.TENANT);
     }
 
     @NonNull
